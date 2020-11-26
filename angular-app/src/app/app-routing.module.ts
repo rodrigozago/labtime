bbe5fa379479c168e8a0a8c8bb23ee720b5a1767
Rodrigo Zago <rodrigozago@me.com>
2020-11-26 11:02:35 -0300

    Updating project to Angular 8

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [{
-  path: '', loadChildren: './registers/registers.module#RegistersModule'
+  path: '', loadChildren: () => import('./registers/registers.module').then(m => m.RegistersModule)
 }];
 
 @NgModule({
